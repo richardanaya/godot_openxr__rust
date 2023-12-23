0252cc5ad1923ed0ba46373a7481115d1f81ebed
RICHΛRD ΛNΛYΛ <richard.anaya@gmail.com>
2023-12-23 00:09:55 -0800

    Update lib.rs

--- rust_openxr/src/lib.rs
+++ rust_openxr/src/lib.rs
@@ -31,18 +31,17 @@ struct RustOpenXR {}
 unsafe impl ExtensionLibrary for RustOpenXR {}
 
 use godot::engine::Node;
+use godot::engine::INode;
+use godot::engine::Material;
+use godot::engine::MeshInstance3D;
+use godot::engine::StandardMaterial3D;
+use godot::engine::XrController3D;
 
 #[derive(GodotClass)]
 #[class(base=Node)]
 struct TriggerColorChanger {
     node: Base<Node>,
 }
-
-use godot::engine::INode;
-use godot::engine::Material;
-use godot::engine::MeshInstance3D;
-use godot::engine::StandardMaterial3D;
-use godot::engine::XrController3D;
 
 #[godot_api]
 impl INode for TriggerColorChanger {
@@ -51,8 +50,10 @@ impl INode for TriggerColorChanger {
     }
 
     fn process(&mut self, _delta: f64) {
+        // our parent is the XRController3D we use to figure out button presses
         let orc: Option<Gd<XrController3D>> = self.node.try_get_node_as("..");
         if let Some(rc) = orc {
+            // our sibling is the MeshInstance3D we use to change the material color
             let om: Option<Gd<MeshInstance3D>> = self.node.try_get_node_as("../ControllerMesh");
             if let Some(m) = om {
                 let osm: Option<Gd<Material>> = m.get_active_material(0);
@@ -65,12 +66,16 @@ impl INode for TriggerColorChanger {
                             stm.set_albedo(Color::from_rgb(1.0, 1.0, 1.0));
                         }
                     } else {
-                        godot_print!("no standard material");
+                        godot_print!("no standard material on mesh");
                     }
+                } else {
+                    godot_print!("no material on mesh");
                 }
+            } else {
+                godot_print!("no controller mesh");
             }
         } else {
-            godot_print!("no controller");
+            godot_print!("no XRController3D parent");
         }
     }
 }
